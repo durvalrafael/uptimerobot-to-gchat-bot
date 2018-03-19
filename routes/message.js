c4e4d3eaf217718983351a57aabac92c64644b05
Durval Moreira <durvalmoreira@durvalmacbook.local>
2018-03-19 17:55:06 -0300

    project commit

---
+++ routes/message.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var router = express.Router();
+var _ = require('lodash');
+
+router.post('/', function (req, res, next) {
+    const object = {
+        stauts: true,
+        query: req.query
+    };
+    
+    const response = _.merge(object, req.body);
+    
+    var json = JSON.stringify(response);
+    var fs = require('fs');
+    fs.writeFile(__dirname + '/data.json', json, 'utf8', function(err){
+        if (err) throw err;
+    });
+
+    res.send(response);
+
+
+});
+
+module.exports = router;
