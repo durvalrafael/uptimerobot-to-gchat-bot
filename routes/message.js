f57b7776539f62cd393147c6bd125cbcb4d58180
Durval <duurval@gmail.com>
2018-04-01 00:36:51 -0300

    #

--- routes/message.js
+++ routes/message.js
@@ -4,16 +4,19 @@ const _ = require('lodash');
 const https = require("https");
 
 router.post('/', function (req, res, next) {
+
     const object = {
         stauts: true,
         query: req.query
     };
     
     const response = _.merge(object, req.body);
+
     let message = `Status Bot: ${response.monitorFriendlyName} is ${response.alertTypeFriendlyName}. Detalhes: ${response.alertDetails}`;
     let json = {
         text: message
-    } 
+    };
+
     console.log("MESSAGE REVIECED" , response);
     console.log("QUERY", req.query);
 
@@ -26,6 +29,7 @@ router.post('/', function (req, res, next) {
             'Content-Type': 'application/json',
         }
     };
+    
     var req = https.request(options, function (res) {
         console.log('Status: ' + res.statusCode);
         console.log('Headers: ' + JSON.stringify(res.headers));
@@ -34,10 +38,11 @@ router.post('/', function (req, res, next) {
             console.log('Body: ' + body);
         });
     });
+
     req.on('error', function (e) {
         console.log('problem with request: ' + e.message);
     });
-    // write data to request body
+
     req.write(JSON.stringify(json));
     req.end();
     
